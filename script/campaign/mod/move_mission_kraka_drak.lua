3791959b2bb27a686a40e686d8578674f5fd3d0a
Tibor Ilić <tilic5@gmail.com>
2020-04-10 22:04:34 +0200

    move mission kraka drak works

--- script/campaign/mod/move_mission_kraka_drak.lua
+++ script/campaign/mod/move_mission_kraka_drak.lua
@@ -5,26 +5,28 @@ mod.move_mission_kraka_drak_payload = function(char)
 	cm:callback(function()
 		CampaignUI.ToggleCinematicBorders(true)
 		cm:stop_user_input(true)
-		cm:scroll_camera_from_current(true, 0.01, {456.56698608398,505.19943237305, 3, d_to_r(0), 3})
+
+		cm:scroll_camera_from_current(true, 0.1, {464.588, 495.165, 5, d_to_r(120), 4})
 		cm:callback(function()
-				cm:scroll_camera_from_current(true, 5, {474.847, 481.216, 5, d_to_r(120), 4})
+			cm:move_to(cm:char_lookup_str(char), 681, 653, true)
+			cm:scroll_camera_from_current(
+				true, 5,
+				{463.25, 505.97, 5, d_to_r(120), 4},
+				{455.23, 504.43, 5, d_to_r(120), 4}
+			)
+			cm:callback(function()
+				CampaignUI.ToggleCinematicBorders(false)
+				cm:stop_user_input(false)
+
+				mod.set_state(mod.states.in_kraka_drak)
+			end, 6)
 		end, 0.5)
-		cm:callback(function()
-				cm:move_to(cm:char_lookup_str(char), 696, 635, true) -- sjoktraken
-				cm:scroll_camera_from_current(true, 3, {456.56698608398,505.19943237305, 5, d_to_r(120), 4})
-				cm:callback(function()
-						CampaignUI.ToggleCinematicBorders(false)
-						cm:stop_user_input(false)
-						mod.set_state(mod.states.in_sjok)
-						core:trigger_event("pj_quests_on_entered_sjoktraken")
-				end, 4)
-		end, 6.5)
 	end, 1)
 end
 
 mod.move_mission_kraka_drak = {
 	key = "move_mission_kraka_drak",
-	ui_offsets = {725, 425+35+2-90-24},
+	ui_offsets = {725-117, 425-257},
 	locs = {
 		title="March to Kraka Drak",
 		desc="March to Kraka Drak",
@@ -33,3 +35,7 @@ mod.move_mission_kraka_drak = {
 	icon = "ui/small_city_schem_frame_major.png",
 	payload = mod.move_mission_kraka_drak_payload,
 }
+
+-- for testing:
+-- local char = cm:get_faction(cm:get_local_faction()):faction_leader()
+-- cm:teleport_to(cm:char_lookup_str(char), 695, 641, false) -- start position at sjok before going to Kraka Drak
