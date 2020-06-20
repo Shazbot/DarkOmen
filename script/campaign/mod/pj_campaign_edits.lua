ae4a7ae768286b9b96fb19f4865575aa3786962c
Tibor Ilić <tilic5@gmail.com>
2020-06-20 22:18:27 +0200

    actually don't abandon karak zorn without a way to get rid of the "no home region" attrition

--- script/campaign/mod/pj_campaign_edits.lua
+++ script/campaign/mod/pj_campaign_edits.lua
@@ -3,6 +3,4 @@ cm:add_first_tick_callback(function()
 	if r and not r:is_null_interface() then
 		cm:change_custom_settlement_name(r:settlement(), "Kraka Ravnsake")
 	end
-
-	cm:set_region_abandoned("wh2_main_southlands_worlds_edge_mountains_karak_zorn")
 end)
