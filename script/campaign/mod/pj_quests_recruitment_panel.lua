2b58669c5311ca5851f839f786218d19c0e8bf62
Tibor Ilić <tilic5@gmail.com>
2020-07-12 19:37:14 +0200

    rewrote the recruitment screen, moving away from UIMF to prevent crashes

--- script/campaign/mod/pj_quests_recruitment_panel.lua
+++ script/campaign/mod/pj_quests_recruitment_panel.lua
@@ -75,7 +75,7 @@ mod.add_unit_to_army = function(unit_key)
 end
 
 --- Keep track of currently visible BoG UI components so we can hide them when turning pages.
-local current_comps = {}
+mod.current_bog_comps = {}
 
 --- Remove units from the mod.bog_pages list that exceeded their recruitment limit.
 mod.remove_invalid_recruitment_entries = function()
@@ -101,36 +101,40 @@ end
 
 --- Draw a page of BoG with our own UI.
 mod.draw_bog_page = function(page_num)
-	local left_list = digForComponent(core:get_ui_root(), "list_left")
-	local right_list = digForComponent(core:get_ui_root(), "list_right")
+	local book_of_grudges = digForComponent(core:get_ui_root(), "book_of_grudges")
+	local left_list = digForComponent(book_of_grudges, "list_left")
+	local right_list = digForComponent(book_of_grudges, "list_right")
 
 	local page_data = mod.bog_pages[page_num]
 	local title = page_data.title
 	local desc = page_data.desc
 
+	local is_left_page = page_num%2 == 1
 	local list = left_list
 	if page_num%2 == 0 then
 		list = right_list
 	end
 
-	local pages = digForComponent(core:get_ui_root(), "pages")
-	local dy_objective = digForComponent(core:get_ui_root(), "dy_objective")
+	local pages = digForComponent(book_of_grudges, "pages")
+	local dy_objective = digForComponent(book_of_grudges, "dy_objective")
 	---@type CA_UIC
-	local description_view = digForComponent(core:get_ui_root(), "description_view")
+	local description_view = digForComponent(book_of_grudges, "description_view")
 	local tx_objectives_header = digForComponent(description_view, "dy_title")
-	local obj = digForComponent(core:get_ui_root(), "objective_copy_"..tostring(page_num))
-	local header = digForComponent(core:get_ui_root(), "tx_objectives_header_copy_"..tostring(page_num))
+	local obj_id = "objective_copy_"..(is_left_page and "left" or "right")
+	local obj = digForComponent(book_of_grudges, obj_id)
+	local header_id = "tx_objectives_header_copy_"..(is_left_page and "left" or "right")
+	local header = digForComponent(book_of_grudges, header_id)
 
 	if not obj then
-		obj = UIComponent(dy_objective:CopyComponent("objective_copy_"..tostring(page_num)))
-		header = UIComponent(tx_objectives_header:CopyComponent("tx_objectives_header_copy_"..tostring(page_num)))
+		obj = UIComponent(dy_objective:CopyComponent(obj_id))
+		header = UIComponent(tx_objectives_header:CopyComponent(header_id))
 		pages:Adopt(header:Address())
 		pages:Adopt(obj:Address())
 	end
 	obj:SetVisible(true)
 	header:SetVisible(true)
-	table.insert(current_comps, obj)
-	table.insert(current_comps, header)
+	table.insert(mod.current_bog_comps, obj)
+	table.insert(mod.current_bog_comps, header)
 
 	local x, y = list:Position()
 	if header then
@@ -147,39 +151,44 @@ mod.draw_bog_page = function(page_num)
 		obj:SetStateText(desc)
 	end
 
-	local img = Util.getComponentWithName("rec_l_img_"..tostring(page_num))
-	if img then
-		img.uic:SetImagePath(page_data.img)
-	else
-		img = Image.new("rec_l_img_"..tostring(page_num), pages, page_data.img)
+	local img_id = "rec_l_img_"..(is_left_page and "left" or "right")
+	local img = digForComponent(book_of_grudges, img_id)
+	if not img then
+		img = UIComponent(pages:CreateComponent(img_id, "ui/templates/custom_image"))
 	end
+	img:SetImagePath(page_data.img, 4)
 	img:Resize(200, 200)
-	img:PositionRelativeTo(list, 190, 180)
+	img:MoveTo(x+190, y+180)
 
-	local rec_button = Util.getComponentWithName("pj_rec_button_"..tostring(page_num)) or TextButton.new("pj_rec_button_"..tostring(page_num), pages, "TEXT", "Recruit")
+	local rec_button_id = "pj_rec_button_"..(is_left_page and "left" or "right")
+
+	local rec_button = digForComponent(book_of_grudges, rec_button_id) or UIComponent(pages:CreateComponent(rec_button_id, "ui/templates/square_large_text_button"))
 	if page_data.limit then
 		local save_value = cm:get_saved_value(page_data.save_id) or 0
 		local num_available_for_recruitment = page_data.limit - save_value
-		rec_button:SetButtonText("Recruit ".."("..tostring(num_available_for_recruitment).." available)")
+
+		local button_txt = UIComponent(rec_button:Find("button_txt"))
+		button_txt:SetStateText("Recruit ".."("..tostring(num_available_for_recruitment).." available)")
 	else
-		rec_button:SetButtonText("Recruit")
+		local button_txt = UIComponent(rec_button:Find("button_txt"))
+		button_txt:SetStateText("Recruit")
 	end
 
-	rec_button:PositionRelativeTo(list, 100+25, 625)
+	rec_button:MoveTo(x+100+25, y+625)
 	rec_button:Resize(350, 50)
 
 	img:SetVisible(true)
 	rec_button:SetVisible(true)
-	table.insert(current_comps, img)
-	table.insert(current_comps, rec_button)
+	table.insert(mod.current_bog_comps, img)
+	table.insert(mod.current_bog_comps, rec_button)
 
-	local rec_button_listener_id = "pj_rec_button_"..tostring(page_num).."_on_click"
+	local rec_button_listener_id = "pj_rec_button_"..rec_button_id.."_on_click"
 	core:remove_listener(rec_button_listener_id)
 	core:add_listener(
 		rec_button_listener_id,
 		"ComponentLClickUp",
 		function(context)
-			return rec_button.uic == UIComponent(context.component);
+			return rec_button == UIComponent(context.component);
 		end,
 		function()
 			if not page_data.payload then return end
@@ -191,12 +200,12 @@ mod.draw_bog_page = function(page_num)
 			if page_data.limit then
 				local save_value = cm:get_saved_value(page_data.save_id) or 0
 				cm:set_saved_value(page_data.save_id, save_value+1)
-				cm:callback(
-					function()
-						mod.remove_invalid_recruitment_entries()
-					end,
-					1
-				)
+				cm:callback(function()
+					local success, ret = pcall(mod.remove_invalid_recruitment_entries)
+					if not success then
+						out(ret)
+					end
+				end, 1)
 			end
 
 			mod.close_book_of_grudges()
@@ -205,7 +214,7 @@ mod.draw_bog_page = function(page_num)
 	)
 
 	if page_data.cost then
-		rec_button.uic:SetTooltipText("Costs "..tostring(page_data.cost).."[[img:icon_money]][[/img]]", true)
+		rec_button:SetTooltipText("Costs "..tostring(page_data.cost).."[[img:icon_money]][[/img]]", true)
 		local local_treasury = cm:get_faction(cm:get_local_faction(true)):treasury()
 		if local_treasury < page_data.cost then
 			rec_button:SetDisabled(true)
@@ -217,6 +226,7 @@ end
 mod.redraw_bog = function(page_num)
 	local book_of_grudges = digForComponent(core:get_ui_root(), "book_of_grudges")
 	if not book_of_grudges then return end
+	if not book_of_grudges:Visible() then return end
 
 	local bog_priority = book_of_grudges:Priority()
 	if bog_priority ~= 200 then
@@ -232,14 +242,15 @@ mod.redraw_bog = function(page_num)
 
 	local l = digForComponent(book_of_grudges, "list_left")
 	local r = digForComponent(book_of_grudges, "list_right")
+	if not l or not r then return end
 	l:SetVisible(false)
 	r:SetVisible(false)
 
-	for _, comp in ipairs(current_comps) do
-		comp:SetVisible(false)
-	end
+	-- for _, comp in ipairs(mod.current_bog_comps) do
+	-- 	comp:SetVisible(false)
+	-- end
 
-	current_comps = {}
+	mod.current_bog_comps = {}
 
 	if mod.bog_pages[page_num] then
 		mod.draw_bog_page(page_num)
@@ -250,6 +261,7 @@ mod.redraw_bog = function(page_num)
 end
 
 mod.on_book_of_grudges_panel_opening = function()
+	mod.remove_invalid_recruitment_entries()
 	local book_of_grudges = digForComponent(core:get_ui_root(), "book_of_grudges")
 	if not book_of_grudges then return end
 	local dy_page = digForComponent(book_of_grudges, "dy_page")
@@ -264,7 +276,10 @@ end
 
 local function init()
 	mod.add_grudges()
-	mod.remove_invalid_recruitment_entries()
+	local success, ret = pcall(mod.remove_invalid_recruitment_entries)
+	if not success then
+		out(ret)
+	end
 
 	-- start running the book of grudges update loop when we open the Book of Grudges panel
 	core:remove_listener("pj_quests_on_book_of_grudges_panel_opened")
@@ -277,12 +292,21 @@ local function init()
 		function()
 			cm:remove_callback("pj_quests_book_of_grudges_update_id")
 			cm:repeat_callback(
-				mod.on_book_of_grudges_panel_opening,
+				function()
+					local success, ret = pcall(mod.on_book_of_grudges_panel_opening)
+
+					if not success then
+						out(ret)
+					end
+				end,
 				0,
 				"pj_quests_book_of_grudges_update_id"
 			)
 
-			mod.on_book_of_grudges_panel_opening()
+			local success, ret = pcall(mod.on_book_of_grudges_panel_opening)
+			if not success then
+				out(ret)
+			end
 		end,
 		true
 	)
@@ -298,6 +322,10 @@ local function init()
 		function()
 			cm:remove_callback("pj_quests_book_of_grudges_update_id")
 			mod.current_bog_page = nil
+			local book_of_grudges = digForComponent(core:get_ui_root(), "book_of_grudges")
+			if book_of_grudges then
+				book_of_grudges:UnLockPriority()
+			end
 		end,
 		true
 	)
@@ -306,3 +334,6 @@ end
 cm:add_first_tick_callback(function()
 	init()
 end)
+if debug.traceback():find('pj_loadfile') then
+	init()
+end
