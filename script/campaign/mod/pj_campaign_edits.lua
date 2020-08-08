9b1010f635649450e6a84440127419a2643c28ec
Tibor Ilić <tilic5@gmail.com>
2020-08-08 18:55:15 +0200

    abandon starting region, set starting gold to a flat amount on every difficulty

--- script/campaign/mod/pj_campaign_edits.lua
+++ script/campaign/mod/pj_campaign_edits.lua
@@ -3,4 +3,9 @@ cm:add_first_tick_callback(function()
 	if r and not r:is_null_interface() then
 		cm:change_custom_settlement_name(r:settlement(), "Kraka Ravnsvake")
 	end
+
+	cm:treasury_mod(cm:get_local_faction(true), -999999)
+	cm:treasury_mod(cm:get_local_faction(true), 5000)
+
+	cm:set_region_abandoned("wh2_main_southlands_worlds_edge_mountains_karak_zorn")
 end)
